b4f711c3fc5f5c8f893152986930857d820b9b14
Julian David Rath <julian.rath@semadox.com>
2023-03-13 15:07:43 +0100

    removed getopt from unistd again.

--- win-headers/unistd.h
+++ win-headers/unistd.h
@@ -13,8 +13,6 @@
 #include <process.h> /* for getpid() and the exec..() family */
 #include <direct.h>  /* for _getcwd() and _chdir() */
 
-// this seems to be needed for test.c in the file source
-#include <getopt.h>
 
 #define srandom srand
 #define random rand
